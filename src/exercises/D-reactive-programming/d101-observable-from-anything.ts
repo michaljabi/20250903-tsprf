49bbb749fb9a16bd6a8eaa8f271c12e55fc8dbda
Jabi <michal.jabi@gmail.com>
2025-09-03 13:03:33 +0200

    task(d101): solved.

--- src/exercises/D-reactive-programming/d101-observable-from-anything.ts
+++ src/exercises/D-reactive-programming/d101-observable-from-anything.ts
@@ -9,13 +9,47 @@ import { Observable } from 'rxjs'
   Pokazanie, że z dowolnych — nawet synchronicznych wartości można utworzyć Observable
 
   #Porównaj:
-  ../C-asynchronous-concepts/c202-promise-from-anything.js
+  ../C-asynchronous-concepts/c201-promise-from-anything.js
 */
 
-  new Observable( (subscriber) => {
+// Provider
+ const number$ =  new Observable<number>( (subscriber) => {
 
     subscriber.next(1234);
+    subscriber.next(1);
+    subscriber.next(678);
+    // subscriber.next('?');
+    subscriber.complete();
   });
+
+
+// Consumer #1:
+number$.subscribe((value) => {
+  console.log(value)
+})
+
+// Consumer #2:
+number$.subscribe((value) => {
+  console.log(value)
+})
+
+// Consumer #3:
+number$.subscribe((value) => {
+  console.log(value)
+})
+
+// Consumer #4:
+number$.subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+  complete: () => {
+    console.log('Completed!')
+  },
+  error: (err) => {
+    console.log(err.message)
+  }
+})
 
 
 export {}
