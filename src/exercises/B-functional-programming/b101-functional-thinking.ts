f2e43fdd8a1559612b5cd71a8d889e3c8b98610d
Jabi <michal.jabi@gmail.com>
2025-09-03 12:09:55 +0200

    task(b101): make it wrok (compare imperative vs declarative - functional)

--- src/exercises/B-functional-programming/b101-functional-thinking.ts
+++ src/exercises/B-functional-programming/b101-functional-thinking.ts
@@ -1,4 +1,3 @@
-
 /**
  #Zadanie:
  1. Zamień pierwsze litery imion na duże i przedstaw jako nowa tablica.
@@ -20,11 +19,47 @@
 */
 
 // Z back-endu przychodzą do nas dane w postaci tablicy stringów:
-const backendApiRequest = (): string[] => ['marian', 'stefan', 'jadwiga', 'henryka', 'anna'];
+const backendApiRequest = (): (string | null)[] => [
+  "marian",
+  "stefan",
+  "jadwiga",
+  "henryka",
+  "anna",
+  null,
+];
 
 // Rozwiązanie:
 const response = backendApiRequest();
 
-console.log(response)
+typeof {}; //=
+typeof []; //=
+typeof null; //=
+typeof ""; //=
+typeof true; //=
 
-export {}
+// (1 === '1'); //=
+// (1 == '1'); //=
+
+// sposób 1: (imperatywny)
+let result: string[] = [];
+for (const name of response) {
+  console.log(name);
+  //console.log(typeof name);
+  if (name === null) {
+    continue;
+  }
+  result.push(name.charAt(0).toUpperCase() + name.slice(1));
+}
+console.log(result);
+
+// sposób 2: (deklaratywny) [podejście funkcyjne]
+
+const upperNames = response
+  .filter((w) => typeof w === "string")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+console.log(upperNames);
+console.log(response);
+// część 2:
+console.log(upperNames.map((name) => name.padStart(20, ".")).join("\n"));
+
+export {};
