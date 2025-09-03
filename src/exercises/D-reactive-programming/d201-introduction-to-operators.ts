022dc8ac2b1e72411f7cf4d887432f8d29ad860d
Jabi <michal.jabi@gmail.com>
2025-09-03 15:55:25 +0200

    task(d201): solved - take, map, filter, tap operators.

--- src/exercises/D-reactive-programming/d201-introduction-to-operators.ts
+++ src/exercises/D-reactive-programming/d201-introduction-to-operators.ts
@@ -1,4 +1,6 @@
-import { interval } from 'rxjs'
+import { interval, Observable } from "rxjs";
+
+import { take, map, filter, tap } from "rxjs/operators";
 
 /**
     #Zadanie:
@@ -11,7 +13,21 @@ import { interval } from 'rxjs'
     naszej subskrypcji
 */
 
-const number$ = interval(800);
+const number$ = interval(2000);
+const hello$: Observable<string> = number$.pipe(
+  take(4),
+  tap((n) => {
+    console.log(n);
+  }),
+  filter((n) => n > 0),
+  map((n) => `Hello world ${n}`),
+  tap((n) => {
+    console.log(n);
+  })
+);
 
+hello$.subscribe((value) => {
+  console.log(value);
+});
 
-export {}
+export {};
