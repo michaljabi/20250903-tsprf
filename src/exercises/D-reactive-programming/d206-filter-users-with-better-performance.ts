7e294a577fdcd8956a2bac9a305649d8d9449271
Jabi <michal.jabi@gmail.com>
2025-09-04 14:25:28 +0200

    task(d206): make it work

--- src/exercises/D-reactive-programming/d206-filter-users-with-better-performance.ts
+++ src/exercises/D-reactive-programming/d206-filter-users-with-better-performance.ts
@@ -1,5 +1,6 @@
-import { li } from '../../dom-api/make-dom'
-import { $ } from '../../dom-api/selector'
+import { from, fromEvent, map, of, filter, tap, startWith } from "rxjs";
+import { li } from "../../dom-api/make-dom";
+import { $ } from "../../dom-api/selector";
 
 // Model danych:
 interface Professional {
@@ -9,20 +10,24 @@ interface Professional {
 }
 
 function userService() {
-  const userFactory = ( name: string, age: number, profession: string ): Professional => ({ name, age, profession });
+  const userFactory = (
+    name: string,
+    age: number,
+    profession: string
+  ): Professional => ({ name, age, profession });
 
   return {
-    getAll: (): Professional[]  => [
-      userFactory( 'Zdzisław Ącki', 48, 'hydraulik' ),
-      userFactory( 'Kasia Kowalska', 28, 'aktorka' ),
-      userFactory( 'Michał Legacy', 58, 'programista' ),
-      userFactory( 'Roman Nowak', 68, 'polityk' ),
-      userFactory( 'Anna Nowak', 25, 'ekonomista' ),
-      userFactory( 'Zdzisław Nowak', 44, 'bankier' ),
-      userFactory( 'Roman Zbych', 35, 'ekonomista' ),
-      userFactory( 'Janina Bosko', 35, 'programista' ),
-    ]
-  }
+    getAll: (): Professional[] => [
+      userFactory("Zdzisław Ącki", 48, "hydraulik"),
+      userFactory("Kasia Kowalska", 28, "aktorka"),
+      userFactory("Michał Legacy", 58, "programista"),
+      userFactory("Roman Nowak", 68, "polityk"),
+      userFactory("Anna Nowak", 25, "ekonomista"),
+      userFactory("Zdzisław Nowak", 44, "bankier"),
+      userFactory("Roman Zbych", 35, "ekonomista"),
+      userFactory("Janina Bosko", 35, "programista"),
+    ],
+  };
 }
 
 /**
@@ -37,14 +42,54 @@ function userService() {
   Pamiętaj o możliwości sprawdzenia działania operatorów na: http://rxmarbles.com/;
 */
 
-  // Pomocnicze DOM makers:
- const makeLi = (user: Professional) => li(`${user.name} (${user.age}) - ${user.profession}`);
+// Pomocnicze DOM makers:
+const makeLi = (user: Professional) =>
+  li(`${user.name} (${user.age}) - ${user.profession}`);
 
-  // Pomocnicze selektory:
-  const userList = $('ul#userList')
-  const inputFilter = $('input[name="filter"]')
+// Pomocnicze selektory:
+const userList = $("ul#userList");
+const inputFilter = $('input[name="filter"]');
 
-  // Rozwiązanie:
+// Rozwiązanie:
 
+// powiąż strumienie:
+const inputText$ = fromEvent(inputFilter, "input").pipe(
+  map((ev) => {
+    // if (ev.target instanceof HTMLInputElement) {
+    //   return ev.target.value;
+    // }
+    return (ev.target as HTMLInputElement).value;
+  })
+);
+const user$ = from(userService().getAll());
 
-export {}
+inputText$
+  .pipe(
+    startWith(""),
+    tap(() => {
+      userList.innerHTML = "";
+    })
+  )
+  .subscribe((text) => {
+    user$
+      .pipe(
+        filter((u) => u.profession.toLowerCase().startsWith(text.toLowerCase()))
+      )
+      .subscribe((user) => {
+        userList.append(makeLi(user));
+      });
+  });
+
+// inputFilter.addEventListener("input", (ev) => {
+//   if (ev.target instanceof HTMLInputElement) {
+//     console.log(ev.target.value);
+//   }
+//   userList.innerHTML = "";
+//   userList.append(
+//     ...userService().getAll().map(makeLi)
+//     // makeLi({ name: "Michał", age: 20, profession: "sprzedawca" }),
+//     // makeLi({ name: "Henryk", age: 20, profession: "sprzedawca" })
+//   );
+// });
+
+export {};
