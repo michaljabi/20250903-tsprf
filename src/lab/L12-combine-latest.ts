cfc85eee0cb58588eea8b31b94855fb52beb0989
Jabi <michal.jabi@gmail.com>
2025-09-05 10:57:21 +0200

    example(combineLatest): recognize source of emission inside subscription.

---
+++ src/lab/L12-combine-latest.ts
@@ -0,0 +1,30 @@
+import { timer, combineLatest } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+
+// timerOne emits first value at 1s
+const timerOne$ = timer(1000).pipe(
+  map((n) => n + 1),
+  startWith(0)
+);
+// timerTwo emits first value at 2s,
+const timerTwo$ = timer(2000).pipe(
+  map((n) => n + 1),
+  startWith(0)
+);
+// timerThree emits first value at 3s
+const timerThree$ = timer(3000).pipe(
+  map((n) => n + 1),
+  startWith(0)
+);
+
+// when one timer emits, emit the latest values from each timer as an array
+combineLatest([timerOne$, timerTwo$, timerThree$]).subscribe(
+  ([timerValOne, timerValTwo, timerValThree]) => {
+    // console.log(
+    //   `Timer One Latest: ${timerValOne},
+    //  Timer Two Latest: ${timerValTwo},
+    //  Timer Three Latest: ${timerValThree}`
+    // );
+    console.log(`${timerValOne}-${timerValTwo}-${timerValThree}`);
+  }
+);
