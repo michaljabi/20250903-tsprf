719679ac663a17777e81a2cccde09a4126eedc69
Jabi <michal.jabi@gmail.com>
2025-09-05 09:22:21 +0200

    task(d207): make it work (and right).

--- src/exercises/D-reactive-programming/d207-what-is-a-subscriber.ts
+++ src/exercises/D-reactive-programming/d207-what-is-a-subscriber.ts
@@ -1,5 +1,6 @@
-import { merge, of } from "rxjs";
+import { fromEvent, map, merge, of, scan, startWith } from "rxjs";
 import { $ } from "../../dom-api/selector";
+import { sum } from "../B-functional-programming/number.helpers";
 
 /**
     #Zadanie:
@@ -21,17 +22,33 @@ const addBtn = $("#btnPlus"); // -1-1-1-1-1-1-1
 const minusBtn = $("#btnSubs"); // -(-1)-(-1)
 const resultDiv = $("#counter");
 
-// Nasz Subskrybent
+const mapTo = (n: number) => map((_) => n);
+
+const one$ = fromEvent(addBtn, "click").pipe(mapTo(1));
+const minusOne$ = fromEvent(minusBtn, "click").pipe(mapTo(-1));
+
+const currentNumber$ = merge(one$, minusOne$).pipe(
+  startWith(10),
+  scan(sum),
+  map((n) => String(n))
+);
+
+// Nasz Subskrybent {} (new Object())
 const subscriber = {
   next(value: string) {
     resultDiv.innerText = value;
   },
+  complete() {
+    console.log(":)");
+  },
 };
 // Rozwiązanie:
-of("2010").subscribe(subscriber);
+currentNumber$.subscribe(subscriber);
 
-merge(of(1), of(20), of(22, 12, 1)).subscribe((v) => {
-  console.log(v);
-});
+// const number$ = merge(of(1), of(20), of(22));
+
+// number$.subscribe((v) => {
+//   console.log(v);
+// });
 
 export {};
