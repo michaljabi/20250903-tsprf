2dc5b5cd8b2464db0924d60da8da22e0daf63cf7
Jabi <michal.jabi@gmail.com>
2025-09-05 15:04:47 +0200

    refactor(angular-sample): search.component - destroyRef to cleanup subscriptions.

--- samples/angular-sample/src/app/search.component.ts
+++ samples/angular-sample/src/app/search.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { AlbumListService } from './album-list.service';
 import { Album } from './album';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-search',
@@ -13,28 +14,33 @@ import { Subscription } from 'rxjs';
     </div>
   `,
 })
-export class SearchComponent implements OnInit, OnDestroy {
-
+export class SearchComponent implements OnInit {
   albumListService = inject(AlbumListService);
+
+  destroyRef = inject(DestroyRef);
 
   // Zadanie: spraw, aby ten sygnał wyświetlał poprawną wartość.
   numberOfAlbums = signal(0);
 
-  private subscription?: Subscription;
+  // private subscription?: Subscription;
+
+  // private onDestroy$ = new Subject<void>();
 
   ngOnInit(): void {
-    
     // nie robie tych obliczeń w komponencie - to serwis mi je ma dostarczyć !
     // this.albumListService.searchAlbum$().subscribe((albums) => {
     //   this.numberOfAlbums.set(albums.length);
     // })
 
-    this.subscription = this.albumListService.numberOfSearchedAlbum$().subscribe(n => this.numberOfAlbums.set(n))
+    this.albumListService
+      .numberOfSearchedAlbum$()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((n) => this.numberOfAlbums.set(n));
   }
 
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe()
-  }
+  // ngOnDestroy(): void {
+  //   this.subscription?.unsubscribe()
+  // }
 
   search(event: Event) {
     const text = (event.target as HTMLInputElement).value;
