05e431a2a33372f2361f5649cdb0757534355aaa
Jabi <michal.jabi@gmail.com>
2025-09-05 14:22:04 +0200

    task(angular-sample): make it work

--- samples/angular-sample/src/app/search.component.ts
+++ samples/angular-sample/src/app/search.component.ts
@@ -1,24 +1,32 @@
-import {Component, inject, signal} from '@angular/core';
-import {AlbumListService} from "./album-list.service";
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AlbumListService } from './album-list.service';
+import { Album } from './album';
 
 @Component({
   selector: 'app-search',
   template: `
     <div class="input-group mb-3">
-        <span class="input-group-text">🔎</span>
-        <input class="form-control" placeholder="wyszukaj" (input)="search($event)" />
-        <div class="py-2 px-4 border rounded rounded-start-0">{{ numberOfAlbums() }}</div>
+      <span class="input-group-text">🔎</span>
+      <input class="form-control" placeholder="wyszukaj" (input)="search($event)" />
+      <div class="py-2 px-4 border rounded rounded-start-0">{{ numberOfAlbums() }}</div>
     </div>
-  `
+  `,
 })
-export class SearchComponent {
-    albumListService = inject(AlbumListService);
+export class SearchComponent implements OnInit {
+  albumListService = inject(AlbumListService);
 
-    // Zadanie: spraw, aby ten sygnał wyświetlał poprawną wartość.
-    numberOfAlbums = signal(0);
+  // Zadanie: spraw, aby ten sygnał wyświetlał poprawną wartość.
+  numberOfAlbums = signal(0);
 
-    search(event: Event) {
-        const text = (event.target as HTMLInputElement).value;
-        this.albumListService.searchInput(text);
-    }
+  ngOnInit(): void {
+    
+    this.albumListService.searchAlbum$().subscribe((albums) => {
+      this.numberOfAlbums.set(albums.length);
+    })
+  }
+
+  search(event: Event) {
+    const text = (event.target as HTMLInputElement).value;
+    this.albumListService.searchInput(text);
+  }
 }
