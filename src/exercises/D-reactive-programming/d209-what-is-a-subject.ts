c32a5ea6ac8895a168949c01fc1076d156a0be03
Jabi <michal.jabi@gmail.com>
2025-09-05 11:08:45 +0200

    refactor(d209): make it right - secure Subject to prevent `.next()` outside the Service !

--- src/exercises/D-reactive-programming/d209-what-is-a-subject.ts
+++ src/exercises/D-reactive-programming/d209-what-is-a-subject.ts
@@ -1,19 +1,22 @@
-import {Observable, Subject, Subscriber} from 'rxjs'
+import { Observable, Subject, Subscriber } from "rxjs";
 
 class MySuperService {
+  // private myData = 'some-static-data';
 
-  myData = 'some-static-data';
-  subject = new Subject<string>();
+  // pole prywatne w JS / TS:
+  #myData = "some-static-data";
+  #subject = new Subject<string>();
 
-  getData (): Subject<string> {
-    return this.subject;
+  getData() {
+    // naprawiamy problem - udostępnienia .next() na zewnątrz (Co potencjalnie tworzy tzw. "many sources of truth")
+    return this.#subject.asObservable();
   }
 
-  fire () {
-
+  fire() {
+    this.#subject.next(this.#myData);
   }
 }
-  /**
+/**
     #Zadanie:
     Kontroluj zachowanie strumienia w klasie MySuperService.
     Utwórz jej instancję i dopisz logikę do metody "fire()".
@@ -26,13 +29,21 @@ class MySuperService {
     Określenie zachowania Subject, który jest za równo Observer jak i Observable.
   */
 
-  // Przypomnij sobie konstrukcję:
-  // Jakie posiada ograniczenia ?
-  new Observable<number>( (observer: Subscriber<number>) => {
+// Przypomnij sobie konstrukcję:
+// Jakie posiada ograniczenia ?
+new Observable<number>((observer: Subscriber<number>) => {
+  observer.next(1234);
+});
 
-    observer.next(1234);
-  });
+// Rozwiązanie:
+const myDIInstance = new MySuperService();
 
-  // Rozwiązanie:
+// myDIInstance.#myData;
 
+myDIInstance.getData().subscribe((value: string) => {
+  console.log(value);
+});
 
+// myDIInstance.getData().next("OOPS!");
+
+myDIInstance.fire();
